67df9a7afd5e7caf39ed05308f9a71cf73a1fc0a
habibhidayatullahh <habibbhidayatullahh1101@gmail.com>
2026-02-09 23:41:05 +0700

    pertama

---
+++ latihansoal1.py
@@ -0,0 +1,16 @@
+def fizzbuzz_pluss(n):
+    for x in range(1, n + 1):
+        if x % 7 == 0 and x % 3 == 0:
+            print("FizzSeven")
+        elif x % 3 == 0 and x % 5 == 0:
+            print("FizzBuzz")
+        elif x % 3 == 0:
+            print("Fizz")
+        elif x % 5 == 0:
+            print("Buzz")
+        elif x % 7 == 0:
+            print("Seven")
+        else:
+            print(x)
+
+fizzbuzz_pluss(21)
